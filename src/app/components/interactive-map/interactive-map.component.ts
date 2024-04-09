abba0d78be8ed118865c5eb526da62496dd0553f
SelmaGuedidi <gueselma7@gmail.com>
2024-04-09 22:49:36 +0100

    Add retrun to usa map button

--- src/app/components/interactive-map/interactive-map.component.ts
+++ src/app/components/interactive-map/interactive-map.component.ts
@@ -1,14 +1,13 @@
 
-import {  Component,ChangeDetectorRef, ElementRef, Renderer2, SimpleChange, ViewChild, inject } from '@angular/core';
+import {  Component,ChangeDetectorRef, ViewChild, inject } from '@angular/core';
 
 
 import { HttpClient } from '@angular/common/http';
 
 import * as USAMapData from '../../../assets/United States of America.json';
 
-import * as california from '../../../assets/California.json';
-import * as newYork from '../../../assets/New York.json';
-import { MapsModule,MapsTooltipService,LegendService , ColorMappingSettings, LayerSettings,ZoomSettings,MapsTheme, Maps, shapeSelected, IShapeSelectedEventArgs, Highlight, MapsTooltip, Marker, ILoadEventArgs, ILoadedEventArgs, MapsComponent } from '@syncfusion/ej2-angular-maps';
+
+import { MapsModule,MapsTooltipService,LegendService , ColorMappingSettings, LayerSettings,ZoomSettings, IShapeSelectedEventArgs,  MapsComponent } from '@syncfusion/ej2-angular-maps';
 import { DataService } from '../../services/data.service';
 
 import {  StateService } from '../../services/state.service';
@@ -17,12 +16,13 @@ import { Feature, Point, GeoJsonProperties } from 'geojson';
 import {
   Observable, of,
 } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-interactive-map',
   standalone: true,
-  imports: [MapsModule],
+  imports: [MapsModule,CommonModule],
   providers:[MapsTooltipService,LegendService,ColorMappingSettings,LayerSettings,ZoomSettings],
   templateUrl: './interactive-map.component.html',
   styleUrl: './interactive-map.component.css'
@@ -46,7 +46,17 @@ statesAQI!: any[];
 USAMapData: any= USAMapData;
 
 statesAQI$: Observable<any[]> = of([]);
-
+ngOnInit(){
+  document.getElementById('category')!.onclick = () => {
+   this.returntoUSAMAP()
+      let button: HTMLElement | null= document.getElementById('button');
+      button!.style.display = 'none';
+   
+      (<HTMLElement>document.getElementById('category')).style.visibility = 'hidden';
+      (<HTMLElement>document.getElementById('text')).innerHTML = '';
+      (<HTMLElement>document.getElementById('symbol')).style.visibility = 'hidden';
+  };
+}
     constructor(private service: DataService, private http: HttpClient,private cdr: ChangeDetectorRef) {
       //  let us$ = this.http.get<any>('assets/counties-albers-10m.json');
         
@@ -117,7 +127,7 @@ statesAQI$: Observable<any[]> = of([]);
                   }
               }
           ];
-          this.refreshMap();
+        
       });
       
          this.LegendOptions={
@@ -149,22 +159,8 @@ statesAQI$: Observable<any[]> = of([]);
         
          
     }
-    private refreshMap(): void {
-      if (this.maps && this.maps.refresh) {
-        this.maps.refresh();
-      }
-    }
-    
-  avgaqi$ = this.service.averageValue$;
-  observationSum$ = this.service.numberOfObservations$;
-  recordsSum = this.service.numberOfRecords$;
-  avgaqiByHour$ = this.service.maxCountByHour$;
-  avgaqiByDay$ = this.service.avgValuesByDay$;
-  pollutionElements$ = this.service.pollutionElements$;
-  avgaqiBySeason$ = this.service.avgValueBySeason$;
-  aqiCategories$ = this.service.categories$;
+   
 
- 
 
   public zoomSettings: object = { enable: false ,maxZoom:20};
   public selectedState: string | null = null;
@@ -173,6 +169,12 @@ statesAQI$: Observable<any[]> = of([]);
     const selectedShape: string = (args.data as any)['name'];
     
     if (!this.selectedState) {
+      let button: HTMLElement | null  = document.getElementById('button'); button!.style.display = 'block'; 
+   
+        (<HTMLElement>document.getElementById('category')).style.visibility = 'visible';
+        (<HTMLElement>document.getElementById('text')).innerHTML = selectedShape;
+        (<HTMLElement>document.getElementById('symbol')).style.visibility = 'visible';
+    
       // No state selected yet, set the selected state
       this.selectedState = selectedShape;
       console.log('this.selectedState (after setting):', this.selectedState);
@@ -236,5 +238,53 @@ statesAQI$: Observable<any[]> = of([]);
   private fetchCountiesData(state: string): Observable<any> {
     return this.http.get<any>(`/assets/${state}.json`);
   }
-}
+  private returntoUSAMAP():void
+{
+  this.layerOptions = [
+    {
+        shapeData: USAMapData,
+        dataSource: this.statesAQI,
+        shapeDataPath: "name",
+        shapePropertyPath: "name",
+        tooltipSettings: {
+            visible: true,
+            valuePath: "name",
+            fill: 'black',
+            textStyle: {
+                color: 'white',
+                fontFamily: 'Times New Roman',
+                fontStyle: 'Sans-serif',
+                fontWeight: 'Bold'
+            },
+            format: '<b>State: ${name}</b><br><b>Average AQI: ${value}</b>'
+        },
+        highlightSettings: {
+            enable: true,
+            fill: '#A3B0D0'
+        },
+        selectionSettings: {
+            enable: true,
+            fill: '#4C515B',
+            opacity: 1
+        },
+        shapeSettings: {
+            highlightColor: "#63B7B7",
+            border: { width: 0.6, color: 'black' },
+            colorValuePath: 'value',
+              colorMapping: [
+              { from: 0, to: 50, color: [ "#FFF176"] },
+              { from: 51, to: 100, color: ["#F9A825"] },
+              { from: 101, to: 150, color: ["#ff7768"] },
+              { from: 151, to: 200, color: ["#B71C1C"] }
+          ],
+       
+            fill: '#ffe4dc'
+        }
+    }
+    
+];
+this.cdr.detectChanges();
+this.selectedState=null
+console.log("aa",this.layerOptions)
+}}
  
