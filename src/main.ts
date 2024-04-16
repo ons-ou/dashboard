c963d14f6726a1e69b694ae9ad7d7f8f7322317f
SelmaGuedidi <gueselma7@gmail.com>
2024-04-16 18:59:32 +0100

    Update liccense key

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {registerLicense} from '@syncfusion/ej2-base'
-registerLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCekx3QXxbf1x0ZFNMY19bQXJPIiBoS35RckVnWXhfeHdWQ2RUU0B/")
+registerLicense("ORg4AjUWIQA/Gnt2UFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5Xd0NiWnpZdHBdTmhb")
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
